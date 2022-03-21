bb462ee58364ba423fdb0b2bf6a6dffcecc467b6
1AndricK <101608506+1AndricK@users.noreply.github.com>
2022-03-20 23:47:34 -0600

    Add files via upload

---
+++ Firts Project.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initia l-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/boostrap.min.css">
+    <title>First Project</title>
+</head>
+<body>
+    <div class="container text-center">
+        <h1>Boostrap5</h1>
+    </div>
+  <script src="js/boostrap.min.js"></script>
+</body>
+</html>
